1eec7146b636e2fd9f549b9a33b6e1e5f38c5701
TianFenglei <fenglei.tian@sci-w.com>
2026-01-07 20:43:49 -0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+python3 sample_video.py \
+    --video-size 720 1280 \
+    --video-length 129 \
+    --infer-steps 1 \
+    --prompt "A cat walks on the grass, realistic style." \
+    --flow-reverse \
+    --use-cpu-offload \
+    --save-path ./results
